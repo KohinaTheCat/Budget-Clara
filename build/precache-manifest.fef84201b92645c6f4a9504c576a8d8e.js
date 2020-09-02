b66fa1cd9931caf85588ad5e2402fcf21601325e
KohinaTheCat <clarachick11@hotmail.com>
2020-09-02 19:08:36 -0400

    don't ignore the build folder ig...

---
+++ build/precache-manifest.fef84201b92645c6f4a9504c576a8d8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe61cb5b6013610ce9e10c4b96f8f559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "577b57088f7fcd4ba684",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "bb659b79f1b2671731ac",
+    "url": "/static/css/main.16cf6de0.chunk.css"
+  },
+  {
+    "revision": "577b57088f7fcd4ba684",
+    "url": "/static/js/2.875b6cc8.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.875b6cc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb659b79f1b2671731ac",
+    "url": "/static/js/main.82ae81a4.chunk.js"
+  },
+  {
+    "revision": "656d4d7afc62bc6708f4",
+    "url": "/static/js/runtime-main.abd116db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
